d1d6222e9dfb3b6247d453a68fb74ffdbfd72067
iZedikus <izedikus555@mail.ru>
2026-05-21 14:39:19 +0700

    init commit

---
+++ src/main/java/ru/stepanov/simulacrum/SimulacrumApplication.java
@@ -0,0 +1,13 @@
+package ru.stepanov.simulacrum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulacrumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimulacrumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/stepanov/simulacrum/SimulacrumApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.stepanov.simulacrum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulacrumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
